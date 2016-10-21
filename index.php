63e843561a7f4a53b9487a705f9d60fd72ddf2b1
Andrian Rada <radaandrian21@gmail.com>
2016-10-21 17:24:13 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> For Testing </title>
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	
+
+
+</head>
+<body>
+	<?php include('nav.php') ?>
+	<?php include('accordion.php') ?>
+</body>
+</html>
