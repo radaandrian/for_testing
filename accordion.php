0364578df1ae4af053a9ecb80af75b750cd2ece3
Andrian Rada <radaandrian21@gmail.com>
2016-10-21 20:35:17 +0800

    Testing github desktop

--- accordion.php
+++ accordion.php
@@ -135,7 +135,7 @@
         <div class="col-sm-9 col-md-9">
             <div class="well">
                 <h1>
-                    Accordion Menu With Icon</h1>
+                    For Testing</h1>
                 Admin Dashboard Accordion Menu
             </div>
         </div>
